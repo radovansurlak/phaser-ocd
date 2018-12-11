3ed9240b890db119cb495ab57bf12724c14fc1a7
radovansurlak <radovansurlak@gmail.com>
2018-12-11 10:04:46 +0000

    removed redundant code

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ import App from './app/App'
 import Intro from './app/views/Intro'
 import Grid from './app/views/Grid'
 
-
-const Foo = { template: '<div>foo</div>' }
-const Bar = { template: '<div>bar</div>' }
-
 const router = new VueRouter({
 	routes: [
 		{ path: '/', component: Intro },
